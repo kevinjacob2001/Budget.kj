a192e6bfa6a062fe63f865a1ab71443d8386ca41
Kevin Jacob <kevinjacob2001@gmail.com>
2020-06-27 13:49:29 +0530

    added a new item to budget controller

--- app.js
+++ app.js
@@ -1,7 +1,76 @@
 //BUDGET CONTROLLER
 var budgetController = (function () {
-  //some code
-})();
+
+  var Expense = function (id, description, value) {
+    this.id = id;
+    this.description = description;
+    this.value = value;
+  }
+
+  var Income = function (id, description, value) {
+    this.id = id;
+    this.description = description;
+    this.value = value;
+  };
+
+  var data = {
+    allItems: {
+      exp: [],
+      inc: []
+    },
+    totals: {
+      exp: 0,
+      inc: 0
+    }
+  }
+
+  return {
+    addItem: function (type, des, val) {
+      var newItem, ID;
+
+
+
+      //created new id
+      if(data.allItems[type].length>0)
+      {
+        ID = data.allItems[type][data.allItems[type].length-1].id+1;
+      }
+else{
+  ID=0;
+}     
+
+      //create new item base don ic or exp type
+      if (type === "exp") {
+        newItem = new Expense(ID, des, val)
+      }
+      else if (type === "inc") {
+        newItem = new Income(ID, des, val)
+      }
+
+
+      //push item to data structure
+      data.allItems[type].push(newItem);
+ 
+//return new item 
+      return newItem;
+    },
+
+    testing:function(){
+      console.log(data)
+    }
+
+
+
+  }
+
+
+
+}
+)();
+
+
+
+
 
 //UI CONTROLLER
 var UIController = (function () {
@@ -27,8 +96,16 @@ var UIController = (function () {
   //some code
 })();
 
+
+
+
+
+
+
+
 //MAIN CONTROLLER
 var controller = (function (budgetCtrl, UICtrl) {
+
   var setupEventListeners = function () {
     var DOM = UICtrl.getDOMstrings();
     document.querySelector(DOM.inputBtn).addEventListener("click", ctrlAddItem);
@@ -41,11 +118,13 @@ var controller = (function (budgetCtrl, UICtrl) {
   };
 
   var ctrlAddItem = function () {
+   var input,newItem;
+   
     //1.get field input data
-    var input = UICtrl.getinput();
-    console.log(input);
+   input = UICtrl.getinput();
 
     //2.add itm to budget controller
+newItem=budgetCtrl.addItem(input.type,input.description,input.value)
 
     //3.add item to user interface
 
