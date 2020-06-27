8ae6023aa70c7b49d2fa1fe36d5fed84d38f77b1
Kevin Jacob <kevinjacob2001@gmail.com>
2020-06-27 11:31:43 +0530

    completed the process of inputting data from the user

--- app.js
+++ app.js
@@ -1,59 +1,65 @@
 //BUDGET CONTROLLER
 var budgetController = (function () {
-
   //some code
 })();
-
-
 
 //UI CONTROLLER
 var UIController = (function () {
+  var DOMstrings = {
+    inputType: ".add__type",
+    inputDesc: ".add__description",
+    inputValue: ".add__value",
+    inputBtn: ".add__btn",
+  };
 
-return{
-
-  getinput:function(){
-    var type=document.querySelector(".add__type").value; //will be either inc or exp
-   var description=document.querySelector(".add__description").value;
-   var value=document.querySelector(".add__value").value;
-  
-   }
-}
-
+  return {
+    getinput: function () {
+      return {
+        type: document.querySelector(DOMstrings.inputType).value, //will be either inc or exp
+        description: document.querySelector(DOMstrings.inputDesc).value,
+        value: document.querySelector(DOMstrings.inputValue).value,
+      };
+    },
+    getDOMstrings: function () {
+      return DOMstrings;
+    },
+  };
   //some code
 })();
 
-
 //MAIN CONTROLLER
 var controller = (function (budgetCtrl, UICtrl) {
-  
-  var ctrlAddItem=function(){
-   
- //1.get field input data
-   
- //2.add itm to budget controller
- 
- //3.add item to user interface
- 
- //4. calculate budget
- 
- //5. display budget
- 
-}
-  
-  document.querySelector(".add__btn").addEventListener("click", ctrlAddItem)
- 
-  document.addEventListener("keypress", function (e) {
+  var setupEventListeners = function () {
+    var DOM = UICtrl.getDOMstrings();
+    document.querySelector(DOM.inputBtn).addEventListener("click", ctrlAddItem);
 
-    if (e.keyCode === 13 || e.which===13) { 
+    document.addEventListener("keypress", function (e) {
+      if (e.keyCode === 13 || e.which === 13) {
+        ctrlAddItem();
+      }
+    });
+  };
 
-      ctrlAddItem();
+  var ctrlAddItem = function () {
+    //1.get field input data
+    var input = UICtrl.getinput();
+    console.log(input);
 
-    }
-  })
+    //2.add itm to budget controller
 
+    //3.add item to user interface
 
+    //4. calculate budget
 
+    //5. display budget
+  };
 
+  return {
+    init: function () {
+      console.log("Application has started");
+      setupEventListeners();
+    },
+  };
 })(budgetController, UIController);
 
-
+controller.init();
